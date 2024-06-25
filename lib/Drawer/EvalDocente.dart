5b9ed92955b621ac35c768818f52f488341b3ca0
AlmaFKt <almafloresram@gmail.com>
2024-06-25 09:48:01 -0600

    chore: Update dependencies and remove unused pdf package

--- lib/Drawer/EvalDocente.dart
+++ lib/Drawer/EvalDocente.dart
@@ -5,8 +5,8 @@ import 'package:pdf/pdf.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:printing/printing.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class EvaluacionDocente extends StatefulWidget {
   const EvaluacionDocente({super.key});
@@ -19,7 +19,8 @@ class _EvaluacionDocenteState extends State<EvaluacionDocente> {
   String? _filePath;
   bool _isLoading = true;
   double _zoomLevel = 1.0;
-  final TransformationController _transformationController = TransformationController();
+  final TransformationController _transformationController =
+      TransformationController();
 
   @override
   void initState() {
@@ -28,38 +29,118 @@ class _EvaluacionDocenteState extends State<EvaluacionDocente> {
   }
 
   Future<void> generateAndSavePDF() async {
-    final pdf = pw.Document();
-    Uint8List pdfContent = await EscribirPdf(pdf);
-    String filePath = await guardarPdf(pdfContent);
-    setState(() {
-      _filePath = filePath;
-      _isLoading = false;
-    });
+    try {
+      final pdf = pw.Document();
+      final Uint8List imageData1 =
+          await loadImageFromAssets('images/EDUCACION.png');
+      final Uint8List imageData2 =
+          await loadImageFromAssets('images/TecNM.png');
+      Uint8List pdfContent = await EscribirPdf(pdf, imageData1, imageData2);
+      String filePath = await guardarPdf(pdfContent);
+      setState(() {
+        _filePath = filePath;
+        _isLoading = false;
+      });
+    } catch (e) {
+      print('Error generating PDF: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error generating PDF')),
+      );
+    }
   }
 
-  Future<Uint8List> EscribirPdf(pw.Document pdf) async {
+  Future<Uint8List> loadImageFromAssets(String path) async {
+    final ByteData data = await rootBundle.load(path);
+    return data.buffer.asUint8List();
+  }
+
+  Future<Uint8List> EscribirPdf(
+      pw.Document pdf, Uint8List imageData1, Uint8List imageData2) async {
     pdf.addPage(pw.Page(
       pageFormat: PdfPageFormat.a4,
       margin: pw.EdgeInsets.all(32),
       build: (pw.Context context) {
         return pw.Column(
           children: <pw.Widget>[
-            pw.Header(
-              level: 0,
-              child: pw.Text("CONSTANCIA DE EVALUACIÓN DOCENTE"),
+            pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.start,
+                children: <pw.Widget>[
+                  pw.Image(pw.MemoryImage(imageData1), width: 120),
+                  pw.SizedBox(width: 20), // Espacio entre las imágenes
+                  pw.Image(pw.MemoryImage(imageData2), width: 120),
+                ]),
+            pw.SizedBox(height: 80),
+            pw.Center(
+              child: pw.Text(
+                "CONSTANCIA DE EVALUACIÓN DOCENTE",
+                style: pw.TextStyle(
+                  fontWeight: pw.FontWeight.bold,
+                ),
+              ),
             ),
-            pw.Paragraph(
-              text:
-                  "Por medio de la presente se hace CONSTAR que la estudiante FLORES RAMIREZ ALMA, número de control 22090633, con Plan de Estudios ISIC-2010-224, contestó en su totalidad el cuestionario de Evaluación Docente de acuerdo a su horario registrado en el sistema, correspondiendo este al período ENERO - JUNIO 2024",
+            pw.SizedBox(height: 90),
+            pw.RichText(
+              text: pw.TextSpan(
+                style: pw.TextStyle(),
+                children: [
+                  pw.TextSpan(
+                    text:
+                        "Por medio de la presente se hace CONSTAR que la estudiante ",
+                  ),
+                  pw.TextSpan(
+                    text: "FLORES RAMIREZ ALMA",
+                    style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                  ),
+                  pw.TextSpan(
+                    text: ", número de control ",
+                  ),
+                  pw.TextSpan(
+                    text: "22090633",
+                    style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                  ),
+                  pw.TextSpan(
+                    text: ", con Plan de Estudios ",
+                  ),
+                  pw.TextSpan(
+                    text: "ISIC-2010-224",
+                    style: pw.TextStyle(fontWeight: pw.FontWeight.bold),
+                  ),
+                  pw.TextSpan(
+                    text:
+                        ", contestó en su totalidad el cuestionario de Evaluación Docente de acuerdo a su horario registrado en el sistema, correspondiendo este al período ENERO - JUNIO 2024",
+                  ),
+                ],
+              ),
             ),
-            pw.Paragraph(
-              text: "Zacatepec, Morelos, a 18 de junio de 2024.",
+            pw.SizedBox(height: 55), 
+            pw.Align(
+              alignment: pw.Alignment.topRight,
+              child: pw.Paragraph(
+                text: ('Zacatepec, Morelos, a 25 de junio de 2024.'),
+                //DateFormat('dd \'de\' MMMM \'de\' yyyy', 'es_MX')
+                    //.format(DateTime.now()),
+              ),
             ),
-            pw.Paragraph(
-              text: "ATENTAMENTE,",
+            pw.SizedBox(height: 70),
+            pw.Align(
+              alignment: pw.Alignment.topLeft,
+              child: pw.Paragraph(
+                text: "ATENTAMENTE,",
+              ),
             ),
-            pw.Paragraph(
-              text: "DEPARTAMENTO DE DESARROLLO ACADÉMICO.",
+            pw.SizedBox(height: 25),
+            pw.Align(
+              alignment: pw.Alignment.topLeft,
+              child: pw.Paragraph(
+                text: "DEPARTAMENTO DE DESARROLLO ACADÉMICO.",
+              ),
+            ),
+            pw.Align(
+              alignment: pw.Alignment.topRight,
+              child: pw.Paragraph(
+                text:
+                    "Calzada Tecnológico No. 27 Col. Centro, C.P.62780 Zacatepec, Morelos, Tel. (734) 3432110y 3432111 Ext. 228 e-mail: desarrollo_academico@itzacatepec.edu.mx | zacatepec.tecnm.mx",
+              ),
             ),
           ],
         );
@@ -69,31 +150,24 @@ class _EvaluacionDocenteState extends State<EvaluacionDocente> {
   }
 
   Future<String> guardarPdf(Uint8List pdfContent) async {
-    Directory directorioDoc = await getApplicationDocumentsDirectory();
-    String documentoPath = directorioDoc.path;
-    String filePath = "$documentoPath/ejemplo.pdf";
-    File archivo = File(filePath);
-    await archivo.writeAsBytes(pdfContent);
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path;
+    String filePath = "$path/ejemplo.pdf";
+    File file = File(filePath);
+    await file.writeAsBytes(pdfContent);
     return filePath;
   }
 
-  Future<void> descargarPdf() async {
-    if (await Permission.storage.request().isGranted) {
-      Directory? externalDir = await getExternalStorageDirectory();
-      String downloadPath = "${externalDir!.path}/ejemplo.pdf";
-      File archivo = File(downloadPath);
-      if (_filePath != null) {
-        File(_filePath!).copy(downloadPath);
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('PDF descargado a $downloadPath')),
-        );
-      }
-    }
-  }
-
   Future<void> imprimirPdf() async {
-    if (_filePath != null) {
-      Printing.layoutPdf(onLayout: (_) => File(_filePath!).readAsBytesSync());
+    try {
+      if (_filePath != null) {
+        Printing.layoutPdf(onLayout: (_) => File(_filePath!).readAsBytesSync());
+      }
+    } catch (e) {
+      print('Error generating PDF: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error generating PDF: $e')),
+      );
     }
   }
 
@@ -108,7 +182,8 @@ class _EvaluacionDocenteState extends State<EvaluacionDocente> {
             onPressed: () {
               setState(() {
                 _zoomLevel += 0.1;
-                _transformationController.value = Matrix4.identity()..scale(_zoomLevel);
+                _transformationController.value = Matrix4.identity()
+                  ..scale(_zoomLevel);
               });
             },
           ),
@@ -117,13 +192,10 @@ class _EvaluacionDocenteState extends State<EvaluacionDocente> {
             onPressed: () {
               setState(() {
                 _zoomLevel -= 0.1;
-                _transformationController.value = Matrix4.identity()..scale(_zoomLevel);
+                _transformationController.value = Matrix4.identity()
+                  ..scale(_zoomLevel);
               });
             },
-          ),
-          IconButton(
-            icon: Icon(Icons.download),
-            onPressed: descargarPdf,
           ),
           IconButton(
             icon: Icon(Icons.print),
--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -27,7 +27,7 @@ class _mobileBodyState extends State<mobileBody> {
     EvaluacionDocente(),
     CambiarPass(),
     ValidarHorario(),
-    CorreosProfesores(), 
+    CorreosProfesores(),
     // otras paginas
   ];
 
@@ -64,27 +64,39 @@ class _mobileBodyState extends State<mobileBody> {
             ),
             UserAccountsDrawerHeader(
               accountEmail: StreamBuilder<User?>(
-    stream: FirebaseAuth.instance.authStateChanges(),
-    builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-      // Asumimos que siempre hay un usuario logueado, por lo tanto, siempre debería haber un correo.
-      final email = snapshot.data?.email ?? 'No disponible';
-      return Text(email);
-    },
-  ),
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+                  final email = snapshot.data?.email ?? 'No disponible';
+                  return Text(email);
+                },
+              ),
               accountName: StreamBuilder<User?>(
-    stream: FirebaseAuth.instance.authStateChanges(),
-    builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-      // Asumimos que siempre hay un usuario logueado, por lo tanto, siempre debería haber un nombre o se muestra 'Estudiante'.
-      final displayName = snapshot.data?.displayName ?? 'Estudiante';
-      return Text(
-        displayName,
-        style: TextStyle(fontSize: 22.0),
-      );
-    },
-  ),
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+                  final displayName =
+                      snapshot.data?.displayName ?? 'Estudiante';
+                  return Text(
+                    displayName,
+                    style: TextStyle(fontSize: 22.0),
+                  );
+                },
+              ),
               decoration: BoxDecoration(
                 color: Color.fromARGB(255, 27, 58, 133),
               ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home_outlined),
+              title: const Text(
+                'Menu',
+                style: TextStyle(fontSize: 17.0),
+              ),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 0;
+                });
+                Navigator.of(context).pop();
+              },
             ),
             ListTile(
               leading: const Icon(Icons.star_half),
@@ -113,9 +125,9 @@ class _mobileBodyState extends State<mobileBody> {
               },
             ),
             ListTile(
-              leading: const Icon(Icons.menu_book),
+              leading: const Icon(Icons.email_outlined),
               title: const Text(
-                'Biblioteca Digital',
+                'Correos Profesores',
                 style: TextStyle(fontSize: 17.0),
               ),
               onTap: () {
@@ -125,31 +137,54 @@ class _mobileBodyState extends State<mobileBody> {
                 Navigator.of(context).pop();
               },
             ),
-            ListTile(
-              leading: const Icon(Icons.email_outlined),
-              title: const Text(
-                'Correos Profesores',
-                style: TextStyle(fontSize: 17.0),
-              ),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 4;
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-            ListTile(
+            ExpansionTile(
               leading: const Icon(CupertinoIcons.question_circle),
               title: const Text(
                 'Consultas',
                 style: TextStyle(fontSize: 17.0),
               ),
-              onTap: () {
-                setState(() {
-                  _currentIndex = 5;
-                });
-                Navigator.of(context).pop();
-              },
+              iconColor: Theme.of(context).colorScheme.tertiary,
+              collapsedIconColor: Theme.of(context).colorScheme.tertiary,
+              textColor: Theme.of(context).colorScheme.tertiary,
+              collapsedTextColor: Theme.of(context).colorScheme.tertiary,
+              children: <Widget>[
+                ListTile(
+                  title: const Text('Horario'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 10;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Boleta'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 11;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Avance reticular'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 12;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Mi tutor'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 13;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
             ),
             ListTile(
               leading: const Icon(CupertinoIcons.person),
@@ -159,7 +194,7 @@ class _mobileBodyState extends State<mobileBody> {
               ),
               onTap: () {
                 setState(() {
-                  _currentIndex = 6;
+                  _currentIndex = 5;
                 });
                 Navigator.of(context).pop();
               },
@@ -172,15 +207,100 @@ class _mobileBodyState extends State<mobileBody> {
               ),
               onTap: () {
                 setState(() {
-                  _currentIndex = 7;
+                  _currentIndex = 6;
                 });
                 Navigator.of(context).pop();
               },
             ),
-            ListTile(
+            ExpansionTile(
               leading: const Icon(Icons.monetization_on_outlined),
               title: const Text(
                 'Recursos financieros',
+                style: TextStyle(fontSize: 17.0),
+              ),
+              iconColor: Theme.of(context).colorScheme.tertiary,
+              collapsedIconColor: Theme.of(context).colorScheme.tertiary,
+              textColor: Theme.of(context).colorScheme.tertiary,
+              collapsedTextColor: Theme.of(context).colorScheme.tertiary,
+              children: <Widget>[
+                ListTile(
+                  title: const Text('Costos servicios'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 14;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Manual proceso de pagos'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 15;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Asignar pago a servicio'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 14;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Referenciar pago'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 15;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Hoja de pago'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 16;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Subir voucher de pago'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 17;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Consulta vouchers'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 18;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ListTile(
+                  title: const Text('Pago de servicios con tarjeta'),
+                  onTap: () {
+                    setState(() {
+                      _currentIndex = 19;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+            ListTile(
+              leading: const Icon(CupertinoIcons.doc_text),
+              title: const Text(
+                'Mis documentos',
                 style: TextStyle(fontSize: 17.0),
               ),
               onTap: () {
@@ -198,7 +318,7 @@ class _mobileBodyState extends State<mobileBody> {
               ),
               onTap: () {
                 setState(() {
-                  _currentIndex = 9;
+                  _currentIndex = 8;
                 });
                 Navigator.of(context).pop();
               },
@@ -211,7 +331,7 @@ class _mobileBodyState extends State<mobileBody> {
               ),
               onTap: () {
                 setState(() {
-                  _currentIndex = 10;
+                  _currentIndex = 9;
                 });
                 Navigator.of(context).pop();
               },
